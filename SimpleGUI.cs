54ed53121e1bd0286ba5ce217bab11ce685837bf
Fewdys <dipzei.vibes@gmail.com>
2023-09-12 10:31:24 -0400

    SimpleGUI.cs

---
+++ SimpleGUI.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+using System;
+using UnityEngine;
+
+namespace SimpleGUI
+{
+    public class SimpleGUI : MelonMod
+    {
+        public override void OnGUI()
+        {
+            try
+            {
+                GUI.Label(new Rect(3f, 0f, 160f, 90f), $"<color=red>Day: </color><color=cyan>{DateTime.Now.DayOfWeek}\n</color>");
+                GUI.Label(new Rect(3f, 12f, 160f, 90f), $"<color=red>Date: </color><color=cyan>{DateTime.Now.ToString("MM/dd/yyyy")}\n</color>");
+                GUI.Label(new Rect(3f, 24f, 160f, 90f), $"<color=red>Time: </color><color=cyan>{DateTime.Now.ToString("h:m:s tt")}\n</color>");
+                GUI.Label(new Rect(3f, 36f, 160f, 90f), $"<color=red>FPS: </color><color=cyan>{(int)(1.0f / Time.smoothDeltaTime)}\n</color>");
+            }
+            catch { MelonLogger.Msg("Failed To Create One Or More Of The GUI Labels - Likely An Issue Due To The Game or Another Mod Interfering"); }
+        }
+    }
+}
